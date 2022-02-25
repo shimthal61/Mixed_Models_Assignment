842e371b7d7f9393afe14a4ec0cacd2bc88aa12f
Harvey Schneider <50023408+shimthal61@users.noreply.github.com>
2022-02-25 12:25:56 +0000

    added lotsssss

--- question_2_script.R
+++ question_2_script.R
@@ -13,3 +13,25 @@ head(q2_data_tidied)
 q2_data_tidied %>% 
   group_by(StoryEmotion, FaceExpression) %>% 
   summarise(mean = mean())
+
+q2_data_tidied %>% 
+  ggplot(aes(x = StoryEmotion:FaceExpression, y = RT, colour = FaceExpression)) +
+  geom_violin(width = 0.6) +
+  geom_point(alpha = 0.2, position = position_jitter(width = 0.1, seed = 42)) +
+  stat_summary(fun.data = 'mean_cl_boot', colour = 'black') +
+  theme_minimal() +
+  scale_y_continuous(breaks = seq(0, 10000, by = 1250),
+                     limits = c(0, 10000)) +
+  scale_x_discrete(labels = c("Anger:Anger" = "Anger",
+                              "Anger:Fear" = "Anger",
+                              "Fear:Anger" = "Fear",
+                              "Fear:Fear" = "Fear")) +
+  labs(x = "Story Emotion",
+       y = "Reaction Time (ms. )",
+       title = "Examining the Effect of Story Emotion\n and Face Expression on Reaction Time") +
+  theme(plot.title = element_text(size = 25, hjust = 0.5, margin = margin(b = 35), face = "bold"),
+        axis.title.x = element_text(size = 20, margin = margin(t = 30)),
+        axis.title.y = element_text(size = 20, margin = margin(r = 30)),
+        text = element_text(family = "lato", size = 15),
+        plot.margin = unit(rep(1.2, 4), "cm")) +
+  coord_flip()
