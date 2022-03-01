28ae664bbe32bc1a8aa0a92fd3923fe090883f79
Harvey Schneider <50023408+shimthal61@users.noreply.github.com>
2022-03-01 13:10:17 +0000

    Added distribution plot

--- question_2_script.R
+++ question_2_script.R
@@ -10,6 +10,12 @@ q2_data_tidied <- q2_data_raw %>%
 
 head(q2_data_tidied)
 
+q2_descriptives <- q2_data_tidied %>% 
+  group_by(StoryEmotion, FaceExpression) %>% 
+  summarise(mean = mean(RT), sd = sd(RT)) %>% 
+  arrange(mean)
+head(q2_descriptives)
+
 q2_data_tidied %>% 
   ggplot(aes(x = StoryEmotion:FaceExpression, y = RT, colour = FaceExpression)) +
   geom_violin() +
@@ -88,5 +94,52 @@ check_model(q2_max_feas_model)
 
 summary(q2_max_feas_model)
 
-emmeans(q2_buildmer_model)
-            
+emmeans(q2_max_feas_model)
+
+head(q2_data_tidied)
+
+# Histogram
+q2_data_tidied  %>% 
+  ggplot(aes(x = RT)) +
+  geom_histogram(aes(y =..density..),
+                 binwidth = 100,
+                 colour = "black", fill = "black",
+                 alpha = 0.8) +
+  theme_minimal() +
+  labs(x = "Reaction Time (ms. )",
+       y = "Density",
+       title = "Density Plot Visualising out Beta Distribution") +
+  theme(plot.title = element_text(size = 40, hjust = 0.5, margin = margin(b = 25), line = 0.5, face = "bold"),
+        axis.title.x = element_text(size = 30, margin = margin(t = 20)),
+        axis.title.y = element_text(size = 30, margin = margin(r = 20)),
+        text = element_text(family = "lato", size = 25))
+
+# Density Plot
+q2_data_tidied  %>% 
+  ggplot(aes(x = RT)) +
+  geom_density(fill = "#FF6666") +
+  theme_minimal() +
+  labs(x = "Reaction Time (ms. )",
+       y = "Density",
+       title = "Density Plot Visualising out Beta Distribution") +
+  theme(plot.title = element_text(size = 40, hjust = 0.5, margin = margin(b = 25), line = 0.5, face = "bold"),
+        axis.title.x = element_text(size = 30, margin = margin(t = 20)),
+        axis.title.y = element_text(size = 30, margin = margin(r = 20)),
+        text = element_text(family = "lato", size = 25))
+
+# Histogram and Density Plot
+q2_data_tidied  %>% 
+  ggplot(aes(x = RT)) +
+  geom_histogram(aes(y =..density..),
+                 binwidth = 100,
+                 colour = "black", fill = "black",
+                 alpha = 0.8) +
+  geom_density(alpha = .5, fill = "#FF6666") +
+  theme_minimal() +
+  labs(x = "Reaction Time (ms. )",
+       y = "Density",
+       title = "Density Plot Visualising out Beta Distribution") +
+  theme(plot.title = element_text(size = 40, hjust = 0.5, margin = margin(b = 25), line = 0.5, face = "bold"),
+        axis.title.x = element_text(size = 30, margin = margin(t = 20)),
+        axis.title.y = element_text(size = 30, margin = margin(r = 20)),
+        text = element_text(family = "lato", size = 25))
