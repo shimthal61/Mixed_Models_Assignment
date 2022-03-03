6fc33eb16f5955496dfb337c51da367a428ce099
Harvey Schneider <50023408+shimthal61@users.noreply.github.com>
2022-03-03 11:22:39 +0000

    new markdown for testing graphs

--- test_visualisations_script.R
+++ test_visualisations_script.R
@@ -28,3 +28,34 @@ q1_data_tidied %>%
   theme(
     text = element_text(family = "lato", size = 25)) +
   coord_flip()
+
+
+
+q2_descriptives <- q2_data_tidied %>% 
+  group_by(StoryEmotion, FaceExpression) %>% 
+  summarise(mean = mean(RT), sd = sd(RT)) %>% 
+  arrange(mean)
+head(q2_descriptives)
+
+q2_descriptives %>% 
+  ggplot(aes(x = StoryEmotion, y = mean)) +
+  geom_line(aes(linetype = FaceExpression, group = FaceExpression, colour = FaceExpression), 
+            alpha =0.8, size = 1.2, show.legend = FALSE) +
+  geom_point(size = 2.6, aes(colour = FaceExpression), shape = 15) +
+  geom_text(size = 8, aes(label = label,
+                          colour = FaceExpression),
+            data = labels,
+            nudge_x = 0.17,
+            nudge_y = 70,
+            lineheight = 1) +
+  guides(colour = 'none') +
+  scale_y_continuous(breaks = seq(1800, 2800, by = 200),
+                     limits = c(1800, 2800)) +
+  labs(x = "Story Emotion",
+       y = "Reaction Time (ms. )",
+       title = "Examining the Interaction Between\nStory Emotion and Face Expression") +
+  theme_hc() +
+  theme(plot.title = element_text(size = 40, hjust = 0.5, line = 0.5, margin = margin(b = 25), face = "bold"),
+        axis.title.x = element_text(size = 30, margin = margin(t = 20)),
+        axis.title.y = element_text(size = 30, margin = margin(r = 20)),
+        text = element_text(family = "lato", size = 25))
